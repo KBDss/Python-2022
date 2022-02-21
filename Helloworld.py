16b6a27cafb5e00131a8ab6dd789df0fc7d460b6
KBDss <josren45@kingshigh.school.nz>
2022-02-22 12:14:04 +1300

    Did my first exercise

---
+++ Helloworld.py
@@ -0,0 +1,2 @@
+#This code prints out hello word
+print("Hello world")
---
+++ Questions.py
@@ -0,0 +1,22 @@
+#1. print out name
+print("Josh Renton")
+#2. print out your teachers name
+print("Luke Smith")
+#3. print out the name of your school
+print("Kings High School")
+#4. print out your name
+print(15)
+#5. print out your favourite game and hours in it
+print("csgo 232 hours")
+#6. print out what year it will be in 25 years
+print(2047)
+#7. print out how old you will be in 100 years
+print(115)
+#8. print the answer to 658 divided by 7
+print(658/7)
+#9. print out how many years ago the moon landing was
+print(53)
+#10. print out how the name of your favourite game
+print("csgo")
+#11. print out your favorite class
+print("DTE")
