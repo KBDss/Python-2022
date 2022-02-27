5ec84107aba7ce181b4c6385879ee28e4235b0d8
KBDss <josren45@kingshigh.school.nz>
2022-02-28 09:53:22 +1300

    despiocable me 2

---
+++ questions3.py
@@ -0,0 +1,23 @@
+#1
+lives = 3
+#2
+login = LittleN03
+#3
+balance = 10000000000000000000000000000000000000000000000000000000000000000000000000000
+#4
+phone = 021234567
+#5
+name = Josh
+#6
+age = 15
+#7
+current_year = 2022
+#8
+print(name)
+#9
+print(age)
+#10
+surname = Renton
+#11
+
+
