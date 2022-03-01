ce7e00cb820b0ec4931ff5e67ffdef101519d4ce
KBDss <josren45@kingshigh.school.nz>
2022-03-02 12:20:47 +1300

    new

---
+++ questions5.py
@@ -0,0 +1,10 @@
+#1
+print("Press any key then press enter")
+#2
+print("Type your name and press enter")
+#3
+print("Type your age press enter")
+#4
+user_name = input("Type your name")
+#5
+user_age = input("Type your age") 
