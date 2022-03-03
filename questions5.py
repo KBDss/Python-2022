c39d1c500a2a90d258adbfe23a49e4f14cc2ebe4
KBDss <josren45@kingshigh.school.nz>
2022-03-03 13:20:45 +1300

    breaing bad

--- questions5.py
+++ questions5.py
@@ -7,4 +7,16 @@ print("Type your age press enter")
 #4
 user_name = input("Type your name")
 #5
-user_age = input("Type your age") 
+user_age = int(input("Type your age"))
+#6
+user_movie = input("What is you favourite movie?")
+#7
+user_book = input("Type the name of a book")
+#8
+user_adjective = input("Type an adjective")
+#9
+user_noun = input("Type an noun")
+#10
+user_verb = input("Type an verb")
+#11
+print(f"Cool, I also like {user_movie} and {user_book}") 
